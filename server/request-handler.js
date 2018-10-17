68fccaa43de88fefb5a98fd4c56ec5d1e1d60977
Casey Shamey <caseyshamey@gmail.com>
2018-10-16 23:28:34 -0400

    Passing half of tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,16 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var url = require('url');
+
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+
+var results = [];
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -23,27 +33,80 @@ var requestHandler = function(request, response) {
   // http://nodejs.org/documentation/api/
 
   // Do some basic logging.
+
+
+  var headers = defaultCorsHeaders;
+
   //
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
 
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  //response.writeHead(statusCode, headers);
+
+
+  if (request.method === 'GET' && request.url === '/classes/messages') {
+    response.writeHead(200, headers);
+    response.end(JSON.stringify({results: results}));
+  }
+
+  if (request.method === 'GET' && request.url !== '/classes/messages') {
+    response.writeHead(404, 'Nope!');
+    response.end();
+  }
+
+
+
+  if (request.method === 'POST' && request.url === '/classes/messages') {
+    var body = [];
+    request.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      results.push(parse.JSON(body));
+    });
+
+    response.writeHead(201, headers);
+    // response.write('hello World');
+    response.end();
+
+
+    // var body = [];
+    // //request.pipe(response);
+
+    // request.on('data', (chunk) => {
+    //   body.push(chunk);
+
+    // }).on('end', () => {
+    //   body = Buffer.concat(body).toString();
+    //   //body.push(JSON.parse(body));
+    //   response.end(body);
+    // });
+
+    // request.on('error', (err) => {
+    //   console.error(err);
+    // });
+
+
+  }
+
+  // } else if (request.method === 'OPTIONS') {
+
+  // }
+
+
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,9 +115,15 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end();
 };
 
+
+// console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+// The outgoing status.
+//var statusCode = 200;
+// See the note below about CORS headers
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -64,10 +133,6 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
+
+exports.requestHandler = requestHandler;
